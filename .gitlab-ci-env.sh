eefd1cb21ee0ba602c16be1a92aa67cf776d90c2
Robert Lemke <robert@flownative.com>
2019-09-09 14:50:19 +0200

    Use wget instead of curl in .gitlab-ci-env.sh

--- .gitlab-ci-env.sh
+++ .gitlab-ci-env.sh
@@ -1,5 +1,5 @@
-BUILD_ENV_MICRO_VERSION=$(curl --silent https://versions.flownative.io/projects/base/channels/stable/versions/micro.txt)
+BUILD_ENV_MICRO_VERSION=$(wget -qO https://versions.flownative.io/projects/base/channels/stable/versions/micro.txt)
 export BUILD_ENV_MICRO_VERSION
 
-BUILD_ENV_BAT_VERSION=$(curl --silent https://versions.flownative.io/projects/base/channels/stable/versions/bat.txt)
+BUILD_ENV_BAT_VERSION=$(wget -qO https://versions.flownative.io/projects/base/channels/stable/versions/bat.txt)
 export BUILD_ENV_BAT_VERSION
