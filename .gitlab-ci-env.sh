26cff44dec311846b720e3b211dc5c4ebbedf6e5
Robert Lemke <robert@flownative.com>
2019-09-09 15:43:49 +0200

    Rename env variable names for build args

--- .gitlab-ci-env.sh
+++ .gitlab-ci-env.sh
@@ -1,5 +1,5 @@
-BUILD_ENV_MICRO_VERSION=$(wget -qO- https://versions.flownative.io/projects/base/channels/stable/versions/micro.txt)
-export BUILD_ENV_MICRO_VERSION
+BUILD_ARG_MICRO_VERSION=$(wget -qO- https://versions.flownative.io/projects/base/channels/stable/versions/micro.txt)
+export BUILD_ARG_MICRO_VERSION
 
-BUILD_ENV_BAT_VERSION=$(wget -qO- https://versions.flownative.io/projects/base/channels/stable/versions/bat.txt)
-export BUILD_ENV_BAT_VERSION
+BUILD_ARG_BAT_VERSION=$(wget -qO- https://versions.flownative.io/projects/base/channels/stable/versions/bat.txt)
+export BUILD_ARG_BAT_VERSION
