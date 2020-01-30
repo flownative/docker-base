1edcf3e950de38c6731e2f85c096c671156db53a
Robert Lemke <robert@flownative.com>
2020-01-30 08:53:12 +0100

    Remove legacy configuration from Github workflows

--- .github/build-env.sh
+++
@@ -1,5 +0,0 @@
-BUILD_ARG_MICRO_VERSION=$(wget -qO- https://versions.flownative.io/projects/base/channels/stable/versions/micro.txt)
-export BUILD_ARG_MICRO_VERSION
-
-BUILD_ARG_BAT_VERSION=$(wget -qO- https://versions.flownative.io/projects/base/channels/stable/versions/bat.txt)
-export BUILD_ARG_BAT_VERSION
