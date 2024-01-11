e1ae03ddbb26f07a5efb4f0e3e80f1d1212949e8
Robert Lemke <robert@flownative.com>
2024-01-11 15:57:53 +0100

    Run logrotate every 5 minutes instead of hourly
    This makes sure that logrotate runs more often in order to make file size problems less likely.

--- root-files/opt/flownative/logrotate/sbin/logrotate-cron.sh
+++ root-files/opt/flownative/logrotate/sbin/logrotate-cron.sh
@@ -2,7 +2,7 @@
 
 while true; do
     currentMinute=$(date +"%M")
-    if [ "${currentMinute}" == "15" ]; then
+    if [ $(expr ${currentMinute} % 5) -eq 0 ]; then
         "${LOGROTATE_BASE_PATH}/sbin/logrotate" "--state=${LOGROTATE_BASE_PATH}/var/status" "--log=${FLOWNATIVE_LOG_PATH}/logrotate.log" --verbose "${LOGROTATE_BASE_PATH}/etc/logrotate.conf"
         sleep 10
     fi
